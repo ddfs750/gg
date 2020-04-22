3d24af5183a9a48b2bf69739de4ef35861789dee
ddfs750 <64174244+ddfs750@users.noreply.github.com>
2020-04-23 01:42:06 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("700774696693792773")
+setInterval(function() {
+channel.send(`الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه`);
+}, 30)
+})
